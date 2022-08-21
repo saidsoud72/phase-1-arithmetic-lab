69d073adf586d85809fca8017e7465f1a0ef0799
saidsoud72 <said.omar@student.moringaschool.com>
2022-08-21 17:04:31 +0300

    this will iclude arithmentic js code

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+// Write your code here
+let num1,num2, num3,num4,multiply;
+num1=31;
+num2=2;
+multiply = num1*num2;
+
+let random = Math.floor(Math.random())+ 1;
+num3=22;
+num4=6;
+let mod= num3%num4;
+let max=Math.max(10,20);
